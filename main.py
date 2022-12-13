cf4dec70868033075d9b380ffb83cae38a7ab899
John <98199268+jecko5150@users.noreply.github.com>
2022-12-13 13:16:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
+import time
 from turtle import Screen
-from right import Paddle
+
+from Scoreboard import Scoreboard
+from ball import Ball
+from paddle import Paddle
 import turtle
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor('black')
 screen.title('Pong!')
-paddles = Paddle()
+screen.tracer(0)
+
+paddle = Paddle()
+ball = Ball()
 
 screen.listen()
+screen.onkeypress(fun=paddle.move_up, key="Up")
+screen.onkeypress(fun=paddle.move_down, key="Down")
+screen.onkeypress(fun=paddle.move_up_l, key="w")
+screen.onkeypress(fun=paddle.move_down_l, key="x")
 
+player_1 = Scoreboard('Player 1', [240, 260])
+player_2 = Scoreboard('Player 2', [-240, 260])
 
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
 
+    #  Detect wall
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    # Detect Paddle
+    if ball.distance(paddle.right) < 50 and ball.xcor() > 320:
+        ball.hit()
+        ball.bounce()
+
+    elif ball.distance(paddle.left) < 50 and ball.xcor() < -320:
+        ball.hit()
+        ball.bounce()
+
+    # Score
+    if ball.xcor() > 380:
+        ball.start_game()
+        player_2.increase_score()
+    if ball.xcor() < -380:
+        player_1.increase_score()
+        ball.start_game()
 
 screen.exitonclick()
