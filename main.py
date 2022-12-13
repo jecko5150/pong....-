3474696b41c63cb161be035c173aaaa54e00dccf
John <98199268+jecko5150@users.noreply.github.com>
2022-12-13 13:20:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ball import Ball
 from paddle import Paddle
 import turtle
 
+# Screen setup
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor('black')
@@ -15,15 +16,18 @@ screen.tracer(0)
 paddle = Paddle()
 ball = Ball()
 
+# Control the paddles 
 screen.listen()
 screen.onkeypress(fun=paddle.move_up, key="Up")
 screen.onkeypress(fun=paddle.move_down, key="Down")
 screen.onkeypress(fun=paddle.move_up_l, key="w")
 screen.onkeypress(fun=paddle.move_down_l, key="x")
 
+# Players.... 
 player_1 = Scoreboard('Player 1', [240, 260])
 player_2 = Scoreboard('Player 2', [-240, 260])
 
+# The game
 game_is_on = True
 while game_is_on:
     time.sleep(ball.move_speed)
@@ -36,6 +40,7 @@ while game_is_on:
         ball.bounce()
 
     # Detect Paddle
+    
     if ball.distance(paddle.right) < 50 and ball.xcor() > 320:
         ball.hit()
         ball.bounce()
@@ -44,7 +49,7 @@ while game_is_on:
         ball.hit()
         ball.bounce()
 
-    # Score
+    # Keeping track of score 
     if ball.xcor() > 380:
         ball.start_game()
         player_2.increase_score()
