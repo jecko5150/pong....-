39694db2f5a8d68685bbfa4ab664acd757050855
John <98199268+jecko5150@users.noreply.github.com>
2022-12-13 13:22:35 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-
+# The pong ball... 
 class Ball(Turtle):
 
     def __init__(self):
@@ -19,14 +19,14 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-
+# Bounce off the top of the screen
     def bounce(self):
         self.y_move *= - 1
-
+# Detect hit and increase ball speed 
     def hit(self):
         self.x_move *= -1
         self.move_speed *= .8
-
+# Reset the ball to center of screen and reset ball speed 
     def start_game(self):
         self.home()
         start = [-1, 1]
